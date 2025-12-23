a1d944dc291917ca2b823665bcd858748bc2d7b2
Nicolas Vilches <vil-nico@hotmail.com>
2025-12-23 14:03:57 -0300

    cambio

--- ambulance/admin.py
+++ ambulance/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from ambulance.models import *
+
+admin.site.register(DepartamentoMedico)
+
+@admin.register(DepartamentoMedico)
+class DepartamentoMedicoAdmin(admin.mobelAdmin):
+    list_display = ("nombre", "nro_dpto", "email_dpto", "fecha_de_creacion")
+    list_display_links = ("nombre",)
+    search_fields = ("nro_dpto",)
+    list_filter = ("fecha_de_creacion",)
+    ordering = ("nro_dpto", "nombre")
+    readonly_fields = ("fecha_de_creacion",)
+
 
 # Register your models here.
